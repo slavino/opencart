9da9c4f5605192b5d99c6d8fcffececd6e4c91ae
Slavomir Hustaty <slavomir.hustaty@gmail.com>
2025-04-09 15:30:12 +0200

    initial set of localized files

---
+++ upload/admin/language/sk-sk/catalog/download.php
@@ -0,0 +1,44 @@
+<?php
+// Heading
+$_['heading_title']     = 'Súbory pre stiahnutie';
+
+// Text
+$_['text_success']      = 'Sťahovanie bolo úspešne upravené!';
+$_['text_list']         = 'Zoznam súborov pre stiahnutie';
+$_['text_add']          = 'Pridať súbory';
+$_['text_edit']         = 'Upraviť súbory';
+$_['text_upload']       = 'Váš súbor bol úspešne nahraný!';
+$_['text_report']       = 'Reporty';
+
+// Column
+$_['column_name']       = 'Názov';
+$_['column_ip']         = 'IP';
+$_['column_account']    = 'Účty';
+$_['column_store']      = 'Obchod';
+$_['column_country']    = 'Krajina';
+$_['column_date_added'] = 'Dátum pridania';
+$_['column_action']     = 'Akcia';
+
+// Entry
+$_['entry_name']        = 'Názov pre stiahnutie';
+$_['entry_filename']    = 'Názov súboru';
+$_['entry_mask']        = 'Maska';
+
+// Help
+$_['help_filename']     = 'Súbory môžete nahrať pomocou tlačidla alebo nahrajte súbor cez FTP do adresára pre sťahovanie a zadajte podrobnosti.';
+$_['help_mask']         = 'Odporúča sa, aby názov súboru a maska boli odlišné, kvôli ochrane pred pokusmi o priame zadanie odkazov na sťahovanie.';
+
+// Error
+$_['error_warning']            = 'Upozornenie: Prosím pozorne skontrolujte formulár kvôli nájdeným chybám!';
+$_['error_permission']  	   = 'Upozornenie: Nemáte oprávnenie pre správu sťahovania!';
+$_['error_name']        	   = 'Názov musí byť dlhší ako 3 znaky a kratší než 64 znakov!';
+$_['error_filename']           = 'Názov súboru musí byť dlhší ako 3 znaky a kratší než 128 znakov!';
+$_['error_filename_character'] = 'Názov súboru môže obsahovať iba znaky a-Z, 0-9, - and _ !';
+$_['error_directory']          = 'Súbory na stiahnutie musia byť v storage/download adresári!';
+$_['error_exists']             = 'Súbor neexistuje!';
+$_['error_upload']             = 'Nahranie súboru je povinný údaj!';
+$_['error_headers_sent']       = 'Chyba: HTTP Hlavičky už boli odoslané!';
+$_['error_mask']               = 'Maska musí byť dlhšia ako 3 znaky a kratšia než 128 znakov!';
+$_['error_mask_character']     = 'Maska môže obsahovať iba znaky a-Z, 0-9, - and _ !';
+$_['error_filetype']           = 'Neplatný typ súboru!';
+$_['error_product']            = 'Upozornenie: Toto sťahovanie nemôže byť vymazané, pretože je teraz priradené k %s produktu(om)!';
